808f0d2ba456cba733420b882d81d6ce7192f7ae
Itamar <itamarmuslvi@gmail.com>
2018-03-12 14:29:11 +0200

    Build The Model Of customer And Movie and add Both Controller and view

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        /// <summary>
+        /// This Method return all customers
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var customers = GetCustomer();
+
+            return View(customers);
+        }
+
+        /// <summary>
+        /// This Method take from the request the id number
+        /// And return the customer with this id 
+        /// </summary>
+        /// <param name="id">Id of customer</param>
+        /// <returns></returns>
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomer().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        /// <summary>
+        /// This method return IEnumerable of customers
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id=1,Name="John Smith" },
+                new Customer {Id=2,Name="Mary Williams" }
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Id=1,Name="Sherk" },
+                new Movie {Id=2, Name="Wall-e" }
+            };
+
+            return movies;
+        }
+    }
+}
