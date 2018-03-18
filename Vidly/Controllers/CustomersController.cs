edd70c92540439f575910e73bde0e6ff80c70ccc
Itamar <itamarmuslvi@gmail.com>
2018-03-18 12:00:08 +0200

    Add form page and update info in customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -40,6 +41,75 @@ namespace Vidly.Controllers
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
+        }
+
+        /// <summary>
+        /// This Action should return a View 
+        /// that include the form 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFromViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        /// <summary>
+        /// This method handle in creation customer
+        /// If id equal to 0 this mean new cutomer 
+        /// else mean exist customer so update
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                //This line save the object in memroy
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;  
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }
+
+            //This line save the object into database
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        /// <summary>
+        /// This method handle when you went change 
+        /// the info of Customer by id
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFromViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
         /// <summary>
